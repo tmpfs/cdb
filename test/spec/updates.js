5c641037bebb55ca3cdc011ca125c09601113083
muji <freeformsystems@gmail.com>
2016-01-19 16:35:12 +0800

    Assert on updates method (_db_updates).

---
+++ test/spec/updates.js
@@ -0,0 +1,41 @@
+var expect = require('chai').expect
+  , Server = require('../../index')
+
+describe('cdb:', function() {
+
+  before(function(done) {
+    var server = Server({server: process.env.COUCH});
+    function onRemove(err) {
+      done(); 
+    }
+    server.db.rm({db: 'mock-updates-db'}, onRemove);
+  })
+
+  it('should get server updates', function(done) {
+    var server = Server({server: process.env.COUCH});
+    var req = server.updates(function(err, res, body) {
+      expect(err).to.eql(null);
+      expect(res).to.be.an('object');
+      expect(body).to.be.an('object');
+      server.db.rm({db: 'mock-updates-db'}, done);
+    })
+    req.once('response', function() {
+      server.db.create({db: 'mock-updates-db'}, function(err, res, body){})
+    })
+  });
+
+  it('should get server updates with opts', function(done) {
+    var server = Server({server: process.env.COUCH});
+    var req = server.updates({qs: {feed: Server.feeds.longpoll}}, function(err, res, body) {
+      expect(err).to.eql(null);
+      expect(res).to.be.an('object');
+      expect(body).to.be.an('object');
+      server.db.rm({db: 'mock-updates-db'}, done);
+    })
+    req.once('response', function() {
+      server.db.create({db: 'mock-updates-db'}, function(err, res, body){})
+    })
+  });
+
+
+});
