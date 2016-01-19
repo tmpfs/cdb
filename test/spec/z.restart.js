0f9c321ed54209a828d72d691c16aa70ee80a4fc
muji <freeformsystems@gmail.com>
2016-01-19 15:20:32 +0800

    Assert on restart method.

---
+++ test/spec/z.restart.js
@@ -0,0 +1,26 @@
+var expect = require('chai').expect
+  , Server = require('../../index')
+
+describe('cdb:', function() {
+
+  it('should restart server', function(done) {
+    var server = Server({server: process.env.COUCH});
+    server.restart(function(err, res, body) {
+      expect(err).to.eql(null);
+      expect(res).to.be.an('object');
+      expect(body).to.be.an('object');
+      setTimeout(done, 5000);
+    })     
+  });
+
+  it('should restart server with opts', function(done) {
+    var server = Server();
+    server.restart({server: process.env.COUCH}, function(err, res, body) {
+      expect(err).to.eql(null);
+      expect(res).to.be.an('object');
+      expect(body).to.be.an('object');
+      setTimeout(done, 5000);
+    })     
+  });
+
+});
