209017fa69f537c567b5bd5883146c16a3ec769a
muji <freeformsystems@gmail.com>
2016-01-21 19:26:55 +0800

    Tidy config test spec.

--- test/spec/config.js
+++ test/spec/config.js
@@ -1,11 +1,20 @@
 var expect = require('chai').expect
   , Server = require('../../index')
+  , username = 'foo'
+  , password = 'bar'
+  , headers = {
+      'Authorization': 'Basic '
+        + new Buffer(username + ':' + password).toString('base64')};
 
 describe('cdb:', function() {
 
   it('should set server config', function(done) {
     var server = Server({server: process.env.COUCH})
-      , opts = {section: Server.sections.admins, key: 'foo', value: 'bar'};
+      , opts = {
+          section: Server.sections.admins,
+          key: username,
+          value: password
+        };
     server.config.set(opts, function(err, res, body) {
       expect(err).to.eql(null);
       expect(res).to.be.an('object');
@@ -18,8 +27,7 @@ describe('cdb:', function() {
     var server = Server({server: process.env.COUCH})
       , opts = {section: Server.sections.cors, key: 'credentials'};
 
-    opts.headers = {
-      'Authorization': 'Basic ' + new Buffer('foo:bar').toString('base64')};
+    opts.headers = headers;
 
     server.config.get(opts, function(err, res, body) {
       expect(err).to.eql(null);
@@ -33,8 +41,7 @@ describe('cdb:', function() {
     var server = Server({server: process.env.COUCH})
       , opts = {section: Server.sections.admins, key: 'foo'};
 
-    opts.headers = {
-      'Authorization': 'Basic ' + new Buffer('foo:bar').toString('base64')};
+    opts.headers = headers;
 
     server.config.rm(opts, function(err, res, body) {
       expect(err).to.eql(null);
