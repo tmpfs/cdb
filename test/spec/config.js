e4cb3cc719f1e5fde7b2e4c72632a0f481d0f806
muji <freeformsystems@gmail.com>
2016-01-21 19:36:52 +0800

    Complete config test spec.

--- test/spec/config.js
+++ test/spec/config.js
@@ -26,9 +26,7 @@ describe('cdb:', function() {
   it('should get server config', function(done) {
     var server = Server({server: process.env.COUCH})
       , opts = {section: Server.sections.cors, key: 'credentials'};
-
     opts.headers = headers;
-
     server.config.get(opts, function(err, res, body) {
       expect(err).to.eql(null);
       expect(res).to.be.an('object');
@@ -37,12 +35,88 @@ describe('cdb:', function() {
     })
   });
 
+  it('should error without section (get)', function(done) {
+    var server = Server({server: process.env.COUCH})
+      , opts = {key: 'mock-key'};
+    opts.headers = headers;
+    server.config.get(opts, function(err) {
+      function fn() {
+        throw err;
+      }
+      expect(fn).throws(/section option is required/i)
+      done();
+    })
+  });
+
+  it('should error without key (get)', function(done) {
+    var server = Server({server: process.env.COUCH})
+      , opts = {section: 'mock-section'};
+    opts.headers = headers;
+    server.config.get(opts, function(err) {
+      function fn() {
+        throw err;
+      }
+      expect(fn).throws(/key option is required/i)
+      done();
+    })
+  });
+
+  it('should error without section (set)', function(done) {
+    var server = Server({server: process.env.COUCH})
+      , opts = {key: 'mock-key'};
+    opts.headers = headers;
+    server.config.set(opts, function(err) {
+      function fn() {
+        throw err;
+      }
+      expect(fn).throws(/section option is required/i)
+      done();
+    })
+  });
+
+  it('should error without key (set)', function(done) {
+    var server = Server({server: process.env.COUCH})
+      , opts = {section: 'mock-section'};
+    opts.headers = headers;
+    server.config.set(opts, function(err) {
+      function fn() {
+        throw err;
+      }
+      expect(fn).throws(/key option is required/i)
+      done();
+    })
+  });
+
+  it('should error without section (rm)', function(done) {
+    var server = Server({server: process.env.COUCH})
+      , opts = {key: 'mock-key'};
+    opts.headers = headers;
+    server.config.rm(opts, function(err) {
+      function fn() {
+        throw err;
+      }
+      expect(fn).throws(/section option is required/i)
+      done();
+    })
+  });
+
+  it('should error without key (rm)', function(done) {
+    var server = Server({server: process.env.COUCH})
+      , opts = {section: 'mock-section'};
+    opts.headers = headers;
+    server.config.rm(opts, function(err) {
+      function fn() {
+        throw err;
+      }
+      expect(fn).throws(/key option is required/i)
+      done();
+    })
+  });
+
   it('should remove server config', function(done) {
     var server = Server({server: process.env.COUCH})
       , opts = {section: Server.sections.admins, key: 'foo'};
-
     opts.headers = headers;
-
     server.config.rm(opts, function(err, res, body) {
       expect(err).to.eql(null);
       expect(res).to.be.an('object');
