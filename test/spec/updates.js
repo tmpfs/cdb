ee0890a40c75c02b206cb9e4a83aa4beef8799ea
muji <freeformsystems@gmail.com>
2016-01-21 14:22:30 +0800

    Fix lint errors.

--- test/spec/updates.js
+++ test/spec/updates.js
@@ -5,7 +5,7 @@ describe('cdb:', function() {
 
   before(function(done) {
     var server = Server({server: process.env.COUCH});
-    function onRemove(err) {
+    function onRemove() {
       done(); 
     }
     server.db.rm({db: 'mock-updates-db'}, onRemove);
@@ -20,20 +20,22 @@ describe('cdb:', function() {
       server.db.rm({db: 'mock-updates-db'}, done);
     })
     req.once('response', function() {
-      server.db.create({db: 'mock-updates-db'}, function(err, res, body){})
+      server.db.create({db: 'mock-updates-db'}, function(){})
     })
   });
 
   it('should get server updates with opts', function(done) {
     var server = Server({server: process.env.COUCH});
-    var req = server.updates({qs: {feed: Server.feeds.longpoll}}, function(err, res, body) {
-      expect(err).to.eql(null);
-      expect(res).to.be.an('object');
-      expect(body).to.be.an('object');
-      server.db.rm({db: 'mock-updates-db'}, done);
-    })
+    var req = server.updates({qs: {feed: Server.feeds.longpoll}},
+      function(err, res, body) {
+        expect(err).to.eql(null);
+        expect(res).to.be.an('object');
+        expect(body).to.be.an('object');
+        server.db.rm({db: 'mock-updates-db'}, done);
+      }
+    )
     req.once('response', function() {
-      server.db.create({db: 'mock-updates-db'}, function(err, res, body){})
+      server.db.create({db: 'mock-updates-db'}, function(){})
     })
   });
 
