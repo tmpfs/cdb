57f1b8b854a4280b38a1a45095669774231d2786
muji <freeformsystems@gmail.com>
2016-01-23 14:50:10 +0800

    Assert on peek(), add flush() method.

--- test/spec/noop.js
+++ test/spec/noop.js
@@ -6,7 +6,6 @@ describe('cdb:', function() {
   it('should make request as noop', function(done) {
     var server = Server()
       , opts = {
-        noop: true,
         method: 'get',
         url: process.env.COUCH,
         headers: {
@@ -15,7 +14,17 @@ describe('cdb:', function() {
         },
         body: JSON.stringify({_id: 'mock-post-document'})
       }
+
+    // clear request cache
+    server.flush();
+
+    expect(server.peek()).to.eql(null);
+
     server.request(opts, {noop: true}, function(err) {
+      expect(server.peek()).to.be.an('object');
+      expect(server.peek().req).to.be.an('object');
+      expect(server.peek().req).to.eql(opts);
+
       expect(err).to.be.instanceof(Error);
       expect(err.status).to.eql(-1);
       expect(err.reason).to.eql('noop');
