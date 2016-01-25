1863c45201d795ae772518769206b08685b58153
muji <freeformsystems@gmail.com>
2016-01-25 09:55:47 +0800

    Update design document test fixture.

--- test/fixtures/design.js
+++ test/fixtures/design.js
@@ -6,6 +6,49 @@ var doc = {
         emit(doc._id, null);
       }
     }
+  },
+  lists: {
+    mock: function(head, req) {
+      start({
+        'headers': {
+          'Content-Type': 'application/json'
+        }
+      });
+      var doc = [], row;
+      while(row = getRow()){
+        doc.push(row);
+      }
+      send(toJSON(doc));
+    }
+  },
+  shows: {
+    mock: function(doc, req) {
+      if (!doc) {
+        return {json: {error: 'missing', reason: 'no document to show'}}
+      } else {
+        return {json: doc}
+      }
+    }
+  },
+  updates: {
+    mock: function(doc, req) {
+      if(!doc){
+        return [null, {code: 400,
+          json:
+            {
+              error: 'missing',
+              reason: 'no document to update'
+            }
+        }];
+      }else{
+        if(req.body) {
+          // assuming json input
+          req.body = JSON.parse(req.body);
+          doc.list.push(req.body);
+        }
+        return [doc, {json: {ok: true}}];
+      }
+    }
   }
 }
 
